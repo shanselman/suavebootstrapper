823a263931c0f98197c171baf0788cfc04e839a4
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2015-04-27 15:12:52 +0200

    Remove FSharp.Data dependency

--- build.fsx
+++ build.fsx
@@ -13,12 +13,6 @@ Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 // Step 2. Use the packages
  
 #r "packages/Suave/lib/net40/Suave.dll"
-#r "packages/FSharp.Data/lib/net40/FSharp.Data.dll"
-#r "packages/FSharp.Charting/lib/net40/FSharp.Charting.dll"
- 
-let ctxt = FSharp.Data.WorldBankData.GetDataContext()
- 
-let data = ctxt.Countries.Algeria.Indicators.``GDP (current US$)``
  
 open Suave // always open suave
 open Suave.Http.Successful // for OK-result
@@ -33,4 +27,4 @@ let serverConfig =
        bindings = [ HttpBinding.mk HTTP IPAddress.Loopback port ]
     }
  
-startWebServer serverConfig (OK (sprintf "Hello World! In 2010 Algeria earned %f " data.[2010]))
+startWebServer serverConfig (OK "Hello World!")
