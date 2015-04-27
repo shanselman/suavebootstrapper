3ef41673f0d4e68292d20b339bec497c59847e45
Scott Hanselman <scott@hanselman.com>
2015-04-27 07:39:23 -0700

    Perhaps absolute path?

--- build.cmd
+++ build.cmd
@@ -2,6 +2,7 @@
 cls
 
 mkdir .paket
+REM TODO - might want to do an IF EXISTS *or* a SHA check
 curl https://github.com/fsprojects/Paket/releases/download/1.2.0/paket.bootstrapper.exe -L --insecure -o .paket\paket.bootstrapper.exe
 
 .paket\paket.bootstrapper.exe prerelease
