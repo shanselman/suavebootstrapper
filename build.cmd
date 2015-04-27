0c1a75a5f7b660d10ad7f7272a90ef0d6a900a62
Steffen Forkmann <sforkmann@gmail.com>
2015-04-27 18:03:17 +0200

    Update build.cmd
    Better don't install prereleases of paket.exe

--- build.cmd
+++ build.cmd
@@ -5,7 +5,7 @@ mkdir .paket
 REM TODO - might want to do an IF EXISTS *or* a SHA check
 curl https://github.com/fsprojects/Paket/releases/download/1.2.0/paket.bootstrapper.exe -L --insecure -o .paket\paket.bootstrapper.exe
 
-.paket\paket.bootstrapper.exe prerelease
+.paket\paket.bootstrapper.exe
 if errorlevel 1 (
   exit /b %errorlevel%
 )
