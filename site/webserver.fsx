3ca952d958b9073e6e427ae43eb8b935c87ed5d1
reidev275 <reidev275@gmail.com>
2016-01-29 16:37:09 -0500

    update to api provided by Suave 1.1.0

--- site/webserver.fsx
+++ site/webserver.fsx
@@ -7,18 +7,17 @@
 
 open Fake
 open Suave
-open Suave.Http.Successful
+open Suave.Successful
 open Suave.Web
-open Suave.Types
 open System.Net
 
-let serverConfig = 
+let serverConfig =
     let port = getBuildParamOrDefault "port" "8083" |> Sockets.Port.Parse
     { defaultConfig with bindings = [ HttpBinding.mk HTTP IPAddress.Loopback port ] }
 
-startWebServer serverConfig 
-    (OK  
-        ("Hello World! It's Suave.io on Azure Websites.<br /><ul>" + 
-          "<li>Sample git Repo: <a href='https://github.com/shanselman/suavebootstrapper'>https://github.com/shanselman/suavebootstrapper</a></li>" +
-          "<li>Intro blog post: <a href='http://www.hanselman.com/blog/RunningSuaveioAndFWithFAKEInAzureWebAppsWithGitAndTheDeployButton.aspx'>http://www.hanselman.com/blog/RunningSuaveioAndFWithFAKEInAzureWebAppsWithGitAndTheDeployButton.aspx</a></li>" +
-          "</ul>"))
+startWebServer serverConfig
+    (OK
+        ("Hello World! It's Suave.io on Azure Websites.<br /><ul>" +
+         "<li>Sample git Repo: <a href='https://github.com/shanselman/suavebootstrapper'>https://github.com/shanselman/suavebootstrapper</a></li>" +
+         "<li>Intro blog post: <a href='http://www.hanselman.com/blog/RunningSuaveioAndFWithFAKEInAzureWebAppsWithGitAndTheDeployButton.aspx'>http://www.hanselman.com/blog/RunningSuaveioAndFWithFAKEInAzureWebAppsWithGitAndTheDeployButton.aspx</a></li>" +
+         "</ul>"))
